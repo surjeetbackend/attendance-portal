b21f1fdc6f77a830bc9abf679f3e0ba9b977ace5
surjeetbackend <backendoffice12@gmail.com>
2025-06-13 15:24:42 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../model/user');
+const User = require('./user');
 const bcrypt = require('bcrypt');
 
 // Register a new employee
