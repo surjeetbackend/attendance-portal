9045574516b1d2aa6f741dcdf2cf6446ad32eb59
surjeetbackend <backendoffice12@gmail.com>
2025-06-13 15:24:31 +0530

    Update attendanc.js

--- attendanc.js
+++ attendanc.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
-const Attendance = require('../model/Attendance');
+const Attendance = require('./Attendance');
 require('dotenv').config();
 
 async function getAddressFromCoords(coords) {
